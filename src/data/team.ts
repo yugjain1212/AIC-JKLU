e9533cac9469f055e7dc6e4d0339b9dd6cc391c0
Pragyansh Mishra <mishrapragyansh2005@gmail.com>
2026-08-24 13:52:41 +0530

    Update website linkden

--- src/data/team.ts
+++ src/data/team.ts
@@ -9,23 +9,23 @@ export interface TeamMember {
 export const TEAM_MEMBERS: TeamMember[] = [
   {
     id: '01',
-    name: 'Niddhi Kachhawa',
+    name: 'Nidhi Kachhawa',
     role: 'Chief Executive Officer',
     image: '/team/final_ceo.png',
-    linkedin: 'https://www.linkedin.com/in/niddhi-kachhawa/',
+    linkedin: 'https://www.linkedin.com/in/nidhi-kachhawa-97492b191/',
   },
   {
     id: '02',
     name: 'Shivam Vijay',
     role: 'Associate Manager',
     image: '/team/final_shivam_sir.png',
-    linkedin: 'https://www.linkedin.com/in/shivam-vijay/',
+    linkedin: 'https://www.linkedin.com/in/shivam-vijay-8048a01ab/',
   },
   {
     id: '03',
     name: 'Divyanshu joshi',
     role: 'Executive at AIC JKLU',
     image: '/team/final_diyanshu_sir.png',
-    linkedin: 'https://www.linkedin.com/in/divyanshu-joshi/',
+    linkedin: 'https://www.linkedin.com/in/divyanshu-joshi-5a5084274/',
   },
 ];
